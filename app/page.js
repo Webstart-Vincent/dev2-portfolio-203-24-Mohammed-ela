ac1b428da92533a61038aaab93e914ce3819988e
Mohammed <elamrani.mohammed95@gmail.com>
2024-01-24 13:30:39 +0100

    update

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 
 
 
-<header className="h-105 flex flex-col md:flex-row justify-between items-center bg-black border-b border-gray px-20 py-5">
+<header className="h-105 flex flex-col md:flex-row justify-between items-center bg-black border-b border-gray px-40 py-5">
   <div className="flex items-center">
     <img src="images/my_logo.png" alt="mon-logo" className='w-35 h-10'/>
   </div>
@@ -24,21 +24,26 @@ export default function Home() {
 </header>
 
 
-      <body className=''>
-        <div className="px-20 pt-20 flex flex-col gap-20">
-          <article className='flex flex-col gap-10'>
+      <div className='bg-indigo-300 flex flex-col px-5 py-10 sm:px-40 sm:flex-row sm:py-10'>
+        <div className="flex flex-col gap-10 sm:gap-20">
+          <article className='flex flex-col gap-10 w-250'>
             <p className='text-base'>Bonjour à tous ! Je suis</p>
-            <h1 className='text-5xl'>Developpeur Web</h1>
+            <h1 className='text-xl text-center sm:text-5xl sm:text-left'>Developpeur Web</h1>
                       
-            <p className='text-xl text-gray md:text-lg'>Bonjour et bienvenue sur mon portfolio en ligne ! Je suis passionné par le développement Web, et ce site est le reflet de mon parcours et de mes réalisations.</p>
-            <p className='text-xl text-gray md:text-lg'>Merci de votre visite !</p>
+            <p className='text-sm md:text-xl'>Bonjour et bienvenue sur mon portfolio en ligne ! Je suis passionné par le développement Web, et ce site est le reflet de mon parcours et de mes réalisations.</p>
+            <p className='text-sm md:text-xl'>Merci de votre visite !</p>
           </article>
 
           <div>
-            <button className="bg-gray hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="window.open('./assets/CV_Mohammed_cmp.pdf', '_blank')"> Télécharger CV</button>
+            <button className="bg-pink text-white font-bold py-2 px-4 rounded" onclick="window.open('./assets/CV_Mohammed_cmp.pdf', '_blank')"> Télécharger CV</button>
           </div>
         </div>
-      </body>
+
+        <div className="flex items-center py-5">
+            <img src="images/ma-photo.jpg" alt="ma-photo-profil" className="h-50 w-50 sm:h-auto sm:w-15"/>
+        </div>
+
+      </div>
 
 
 
