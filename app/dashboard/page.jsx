d190c6e4789fab90ff03de077ad9c91895650c4c
Mohammed <elamrani.mohammed95@gmail.com>
2024-02-28 17:07:14 +0100

    add admin secure 2

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,16 @@
 'use client'
-import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'; // Importer useRouter
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
-  const router = useRouter();
+  const router = useRouter(); // Utiliser le hook useRouter
 
-  useEffect(() => {
-    // Si la session n'est pas chargée ou si l'utilisateur n'est pas connecté, rediriger vers la page d'accueil
-    if (status !== 'loading' && !session) {
-      router.push('/'); // Remplacez '/' par le chemin de votre page d'accueil ou de connexion
-    }
-  }, [session, status, router]);
+  // Rediriger vers la page d'accueil si l'utilisateur n'est pas connecté
+  if (status === 'unauthenticated') {
+    router.push('/'); // Remplacez '/' par le chemin de votre page d'accueil si différent
+    return null; // Renvoie null pour éviter d'afficher un contenu avant la redirection
+  }
 
   // Vérifier si la session est en cours de chargement
   if (status === 'loading') {
