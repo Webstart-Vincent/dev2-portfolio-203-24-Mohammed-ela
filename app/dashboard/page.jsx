9a94d37ca5667bead4522bf182dbe23c05fbf094
Mohammed <elamrani.mohammed95@gmail.com>
2024-02-28 16:46:56 +0100

    add admin secure

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,31 +1,18 @@
-// import { useSession } from 'next-auth/react';
-
-// const Dashboard = () => {
-//   const { data: session, status } = useSession();
-
-//   // Vérifier si la session est en cours de chargement
-//   if (status === 'loading') {
-//     return <p>Chargement en cours...</p>;
-//   }
-
-//   // Vérifier si la session est définie
-//   const username = session?.user?.name;
-
-//   // Afficher le contenu du tableau de bord
-//   return (
-//     <>
-//       <h1>Tableau de bord</h1>
-//       <p>Utilisateur : {username !== undefined ? <><span>{username}</span><br></br> <a href='http://localhost:3000/api/auth/signout'>Déconnexion</a></> : <>Pas d'utilisateur <br></br> <a href='http://localhost:3000/api/auth/signin'>Connexion</a></>}</p>
-//     </>
-//   );
-// };
-
-// export default Dashboard;
 'use client'
+import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Si la session n'est pas chargée ou si l'utilisateur n'est pas connecté, rediriger vers la page d'accueil
+    if (status !== 'loading' && !session) {
+      router.push('/'); // Remplacez '/' par le chemin de votre page d'accueil ou de connexion
+    }
+  }, [session, status, router]);
 
   // Vérifier si la session est en cours de chargement
   if (status === 'loading') {
