7d121991700ca33b08af37871ec4e46eacf0c8d7
Mohammed <elamrani.mohammed95@gmail.com>
2024-05-15 11:47:00 +0200

    amelioration front animation

--- app/page.js
+++ app/page.js
@@ -1,41 +1,40 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import Header from '@/components/Header';
+import Header from '@/components/header.jsx';
 import About from '@/components/About';
 import Portfolio from '@/components/Portfolio';
+
 export default function Home() {
     const [works, setWorks] = useState([]);
 
     useEffect(() => {
         //list des projets
-      async function loadProjects() {
-          try {
-              const response = await fetch('/api/projects');
-              if (!response.ok) {
-                  throw new Error('Network response was not ok ' + response.statusText);
-              }
-              const text = await response.text(); 
-              try {
-                  const projects = JSON.parse(text); 
-                  setWorks(projects);
-              } catch (error) {
-                  console.error("Failed to parse JSON:", text, error);
-              }
-          } catch (error) {
-              console.error("Failed to fetch projects:", error);
-          }
-      }
-      loadProjects();
-  }, []);
-  
+        async function loadProjects() {
+            try {
+                const response = await fetch('/api/projects');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                const text = await response.text(); 
+                try {
+                    const projects = JSON.parse(text); 
+                    setWorks(projects);
+                } catch (error) {
+                    console.error("Failed to parse JSON:", text, error);
+                }
+            } catch (error) {
+                console.error("Failed to fetch projects:", error);
+            }
+        }
+        loadProjects();
+    }, []);
 
     return (
         <>
-            <Header />
-            <main>
-                <About />
-                {/* on passe des params au components */}
-                <Portfolio works={works} />
+            <Header className="animate-fadeIn" />
+            <main className="animate-fadeIn">
+                <About className="animate-slideIn" />
+                <Portfolio works={works} className="animate-slideIn" />
             </main>
         </>
     );
--- components/portfolio.jsx
+++ components/portfolio.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import Spinner from '@/components/spinner.js';
 import Link from 'next/link';
 import { CldImage } from 'next-cloudinary';
+
 export default function Portfolio({ works }) {
-    // les parametre sont passé
     return (
-        <section id='projets' name='portfolio' className='h-full grid gap-14 px-10 sm:mb-15 sm:px-10'>
+        <section id='projets' name='portfolio' className='h-full grid gap-14 px-10 sm:mb-15 sm:px-10 animate-fadeIn'>
             <div className='header grid gap-4'>
-                <h3 className='text-3xl font-bold text-center sm:text-left '>Mes derniers projets</h3>
-                <p className='text-gray sm:text-center'>
-            Découvrez une sélection de mes derniers projets, conçus avec passion et expertise pour des clients variés ou pour des projets personnels. Explorez mon portfolio et plongez dans l'univers de mes créations, où chaque projet raconte une histoire unique et témoigne de mon engagement envers l'excellence et l'innovation dans le domaine du développement web.
-           </p>
+                <h3 className='text-3xl font-bold text-center sm:text-left animate-slideIn'>Mes derniers projets</h3>
+                <p className='text-gray sm:text-center animate-slideIn'>
+                    Découvrez une sélection de mes derniers projets, conçus avec passion et expertise pour des clients variés ou pour des projets personnels. Explorez mon portfolio et plongez dans l'univers de mes créations, où chaque projet raconte une histoire unique et témoigne de mon engagement envers l'excellence et l'innovation dans le domaine du développement web.
+                </p>
             </div>
             <div name='gallery' className='sm:pb-8'>
-                <ul className='flex flex-col items-center justify-center gap-4 sm:flex-row'>
+                <ul className='flex flex-col items-center justify-center gap-4 sm:flex-row flex-wrap'>
                     {works.length > 0 ? (
                         works.map((work) => (
-                            <li key={work._id} className='relative border border-white shadow-lg rounded-lg hover:scale-105 transition-transform duration-500 ease-in-out'>
+                            <li key={work._id} className='relative border border-white shadow-lg rounded-lg hover:scale-105 transition-transform duration-500 ease-in-out animate-slideIn'>
                                 <Link href={`/works/${work.slug}`} passHref>
-                                <span name='title-projet' className="absolute top-5 left-5 bg-blackbg p-2 rounded text-gray font-bold">{work.titre}</span>
-                                        <CldImage
-                                            src={work.image}
-                                            alt={work.titre}
-                                            width={500} 
-                                            height={350} 
-                                            className="rounded-lg border-double hover:scale-105 transition-transform duration-500 ease-in-out"
-                                        />
+                                    <span name='title-projet' className="absolute top-5 left-5 bg-blackbg p-2 rounded text-gray font-bold transition-transform transform hover:scale-110 duration-300">{work.titre}</span>
+                                    <CldImage
+                                        src={work.image}
+                                        alt={work.titre}
+                                        width={500}
+                                        height={350}
+                                        className="rounded-lg border-double hover:scale-105 transition-transform duration-500 ease-in-out"
+                                    />
                                 </Link>
                             </li>
                         ))
                     ) : (
-                      <Spinner />
+                        <Spinner />
                     )}
                 </ul>
             </div>
