a454d022299d40c806e683b81b24a66d3d14588d
Mohammed <elamrani.mohammed95@gmail.com>
2024-05-29 14:00:53 +0200

    lighhous

--- components/header.jsx
+++ components/header.jsx
@@ -44,12 +44,14 @@ export default function Header() {
         >
           {menuOpen ? (
             <svg
+            
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
+              <title>menu burger</title>
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
